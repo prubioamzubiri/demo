b014174fa5d817e869115cc4fc26a6e9a70a4614
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-12-09 10:12:24 +0100

    Proyecto creado

---
+++ src/test/java/com/example/PersonaTest.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
+public class PersonaTest {
+
+    @org.junit.Test
+    public void testHBirthday() {
+        System.out.println("hBirthday");
+        
+        Random random = new Random();
+        int initAge = random.nextInt();
+        
+        Persona instance = new Persona("a", "b", initAge, false);
+        instance.hBirthday();
+        int expResultD = initAge;
+        int resultD = instance.getAge();
+
+        instance.setAlive(true);
+        instance.hBirthday();
+        
+        int expResultA = initAge + 1;
+        int resultA = instance.getAge();        
+        
+        assertEquals(expResultD, resultD);
+        assertEquals(expResultA, resultA);
+    }
+
+}
